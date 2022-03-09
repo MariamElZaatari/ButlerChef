0efcfc7bf7f49b24d220c62640824b7624120e9a
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-09 21:53:09 +0200

    Folders Frontend and Backend Added

---
+++ butlerchef_backend/src/test/java/com/example/butlerchef_backend/ButlerchefBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.butlerchef_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButlerchefBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
