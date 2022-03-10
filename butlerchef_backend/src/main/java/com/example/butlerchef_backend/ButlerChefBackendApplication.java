7df8356e6fe30fadf49bf20be34a9757016c60f5
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-10 20:49:32 +0200

    User Model Added

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/ButlerChefBackendApplication.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/ButlerChefBackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ButlerchefBackendApplication {
+public class ButlerChefBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ButlerchefBackendApplication.class, args);
+		SpringApplication.run(ButlerChefBackendApplication.class, args);
 	}
+
+
 
 }
---
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Models/User.java
@@ -0,0 +1,128 @@
+package com.example.butlerchef_backend.Models;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private String image;
+    private int role_id;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public User() {
+    }
+
+    public User(Long id, String email, String password, String first_name, String last_name, String image, int role_id, LocalDateTime created_at, LocalDateTime updated_at) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.image = image;
+        this.role_id = role_id;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public User(String email, String password, String first_name, String last_name, String image, int role_id, LocalDateTime created_at, LocalDateTime updated_at) {
+        this.email = email;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.image = image;
+        this.role_id = role_id;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", image='" + image + '\'' +
+                ", role_id=" + role_id +
+                ", created_at=" + created_at +
+                ", updated_at=" + updated_at +
+                '}';
+    }
+}
