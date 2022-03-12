307fd9527bea4b7bed9720da4cc003902f53c070
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-12 22:45:55 +0200

    FilterRegistrationBean Function Added for AuthFilter Implementation

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/ButlerChefBackendApplication.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/ButlerChefBackendApplication.java
@@ -1,7 +1,10 @@
 package com.example.butlerchef_backend;
 
+import com.example.butlerchef_backend.Filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ButlerChefBackendApplication {
@@ -10,6 +13,14 @@ public class ButlerChefBackendApplication {
 		SpringApplication.run(ButlerChefBackendApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
 
 
 }
