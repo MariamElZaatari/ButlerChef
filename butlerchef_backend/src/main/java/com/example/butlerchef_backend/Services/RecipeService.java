51127c5ad47599fe369fc3396e8c8201b604d3a5
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-12 17:23:43 +0200

    Recipe Service Added

---
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Services/RecipeService.java
@@ -0,0 +1,41 @@
+package com.example.butlerchef_backend.Services;
+
+import com.example.butlerchef_backend.Models.Recipe;
+import com.example.butlerchef_backend.Models.User;
+import com.example.butlerchef_backend.Repositories.RecipeRepository;
+import com.example.butlerchef_backend.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class RecipeService {
+
+    private final RecipeRepository recipeRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public RecipeService(RecipeRepository recipeRepository, UserRepository userRepository) {
+        this.recipeRepository = recipeRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Recipe createRecipe(Recipe recipe) {
+        //add validation
+        Optional<User> temp_user=userRepository.findById(recipe.getUser().getId());
+        User user=temp_user.get();
+        recipe.setUser(user);
+
+        return recipeRepository.save(recipe);
+    }
+
+    public Collection<Recipe> getRecipesByName(String name){
+        return recipeRepository.findByNameLikeIgnoreCase("%"+name+"%");
+    }
+
+    public Collection<Recipe> getRecipesByUserId(Long id){
+        return recipeRepository.findByUserId(id);
+    }
+}
