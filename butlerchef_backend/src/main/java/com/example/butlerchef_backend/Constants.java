222f04474eaf2fc0a9ed0b6ff4087f082120adf9
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-12 22:46:30 +0200

    Contants for JWT Added

---
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Constants.java
@@ -0,0 +1,9 @@
+package com.example.butlerchef_backend;
+
+public class Constants {
+
+    public static final String API_SECRET_KEY = "butlerChef";
+
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 298261;
+
+}
