61d83ab825b2f74a4dc8d946362023d42474bac7
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-12 23:01:27 +0200

    Quantity Model Added

---
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Models/Quantity.java
@@ -0,0 +1,72 @@
+package com.example.butlerchef_backend.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table
+public class Quantity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty(message = "Value cannot be Empty or Null")
+    private String value;
+    private String created_at;
+    private String updated_at;
+
+    @ManyToMany(mappedBy = "quantities")
+
+    private final Set<Measurement> measurements = new HashSet<>();
+
+    public Set<Measurement> getMeasurements() {
+        return measurements;
+    }
+
+    public Quantity(){
+        this.setCreated_at();
+        this.setUpdated_at();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        Date now = new Date();
+        this.created_at = sdf.format(now);
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        Date now = new Date();
+        this.updated_at = sdf.format(now);
+    }
+}
