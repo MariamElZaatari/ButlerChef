709763b7d6fc4c7a1d1a8701f14db155167d26db
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-12 22:50:11 +0200

    User Model Validation Added, and Address and UserRole Relationship Added

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/Models/User.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Models/User.java
@@ -3,10 +3,10 @@ package com.example.butlerchef_backend.Models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
-
 
 @Entity
 @Table
@@ -15,17 +15,54 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Email(message = "Invalid Email")
+    @NotEmpty(message = "Password is mandatory.")
     private String email;
+
+    @Pattern(regexp="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",message = "Invalid Password")
+    @NotEmpty(message = "Password is mandatory.")
     private String password;
+
+    @Pattern(regexp="^[A-Za-z]*$",message = "Invalid First Name")
+    @NotEmpty(message = "Invalid First Name")
     private String first_name;
+
+    @Pattern(regexp="^[A-Za-z]*$",message = "Invalid Last Name")
+    @NotEmpty(message = "Invalid Last Name")
     private String last_name;
+
     private String image;
-    private int role_id;
+
     private String created_at;
     private String updated_at;
 
     @OneToMany(mappedBy = "user")
     private Collection<Recipe> recipes;
+
+    @OneToMany(mappedBy = "user")
+    private Collection<Address> addresses;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    @JsonIgnoreProperties({"created_at","updated_at"})
+    private UserRole userRole;
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public Collection<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Collection<Address> addresses) {
+        this.addresses = addresses;
+    }
 
     public Collection<Recipe> getRecipes() {
         return recipes;
@@ -45,26 +82,24 @@ public class User {
         this.password = password;
     }
 
-    public User(Long id, String email, String password, String first_name, String last_name, String image, int role_id) {
+    public User(Long id, String email, String password, String first_name, String last_name, String image) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.first_name = first_name;
         this.last_name = last_name;
         this.image = image;
-        this.role_id = role_id;
         this.setCreated_at();
         this.setUpdated_at();
 
     }
 
-    public User(String email, String password, String first_name, String last_name, String image, int role_id) {
+    public User(String email, String password, String first_name, String last_name, String image) {
         this.email = email;
         this.password = password;
         this.first_name = first_name;
         this.last_name = last_name;
         this.image = image;
-        this.role_id = role_id;
         this.setCreated_at();
         this.setUpdated_at();
     }
@@ -117,14 +152,6 @@ public class User {
         this.image = image;
     }
 
-    public int getRole_id() {
-        return role_id;
-    }
-
-    public void setRole_id(int role_id) {
-        this.role_id = role_id;
-    }
-
     public String getCreated_at() {
         return created_at;
     }
@@ -154,7 +181,6 @@ public class User {
                 ", first_name='" + first_name + '\'' +
                 ", last_name='" + last_name + '\'' +
                 ", image='" + image + '\'' +
-                ", role_id=" + role_id +
                 ", created_at=" + created_at +
                 ", updated_at=" + updated_at +
                 '}';
