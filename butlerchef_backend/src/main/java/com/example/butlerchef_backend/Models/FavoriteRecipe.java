feb00bd560d5e6c99be93b8e48416a78acab63b5
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-16 01:18:08 +0200

    FavoriteRecipe Model Modified

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/Models/FavoriteRecipe.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Models/FavoriteRecipe.java
@@ -1,67 +1,103 @@
 package com.example.butlerchef_backend.Models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Entity
-@DiscriminatorValue("2")
-public class FavoriteRecipe extends Recipe{
+public class FavoriteRecipe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 
-    private String created_at;
-    private String updated_at;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "recipe_id", referencedColumnName = "id")
+    @JsonIgnoreProperties({"created_at","updated_at"})
+    private Recipe recipe;
+
+    private String createdAt;
+    private String updatedAt;
 
     public FavoriteRecipe() {
-
+        this.setCreatedAt();
+        this.setUpdatedAt();
     }
 
-    @Override
+
+//    @Override
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    @Override
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public FavoriteRecipe(Long id, User user, String name, double rate, String level, String time, String serving, String image, int visibility, Long id1, String created_at, String updated_at) {
+//        super(id, user, name, rate, level, time, serving, image, visibility);
+//        this.id = id1;
+//        this.setCreated_at();
+//        this.setUpdated_at();
+//    }
+
+//    public FavoriteRecipe(User user, String name, double rate, String level, String time, String serving, String image, int visibility, Long id, String created_at, String updated_at) {
+//        super(user, name, rate, level, time, serving, image, visibility);
+//        this.id = id;
+//        this.setCreated_at();
+//        this.setUpdated_at();
+//    }
+
+
     public Long getId() {
         return id;
     }
 
-    @Override
     public void setId(Long id) {
         this.id = id;
     }
 
-    public FavoriteRecipe(Long id, User user, String name, double rate, String level, String time, String serving, String image, int visibility, Long id1, String created_at, String updated_at) {
-        super(id, user, name, rate, level, time, serving, image, visibility);
-        this.id = id1;
-        this.setCreated_at();
-        this.setUpdated_at();
+    public long getUser() {
+        return user.getId();
     }
 
-    public FavoriteRecipe(User user, String name, double rate, String level, String time, String serving, String image, int visibility, Long id, String created_at, String updated_at) {
-        super(user, name, rate, level, time, serving, image, visibility);
-        this.id = id;
-        this.setCreated_at();
-        this.setUpdated_at();
+    public void setUser(User user) {
+        this.user = user;
     }
 
-    public String getCreated_at() {
-        return created_at;
+    public Recipe getRecipe() {
+        return recipe;
     }
 
-    public void setCreated_at() {
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
         Date now = new Date();
-        this.created_at = sdf.format(now);
+        this.createdAt = sdf.format(now);
     }
 
-    public String getUpdated_at() {
-        return updated_at;
+    public String getUpdatedAt() {
+        return updatedAt;
     }
 
-    public void setUpdated_at() {
+    public void setUpdatedAt() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
         Date now = new Date();
-        this.updated_at = sdf.format(now);
+        this.updatedAt = sdf.format(now);
     }
 }
