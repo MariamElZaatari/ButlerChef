f669cf9de14c462bfc5d7e38bdb4aa91c4287c8c
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-16 18:42:09 +0200

    Recipe Service Update Method Added

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/Services/RecipeService.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.example.butlerchef_backend.Services;
 
+import com.example.butlerchef_backend.Models.Address;
 import com.example.butlerchef_backend.Models.Recipe;
 import com.example.butlerchef_backend.Models.User;
 import com.example.butlerchef_backend.Repositories.RecipeRepository;
@@ -23,7 +24,6 @@ public class RecipeService {
     }
 
     public Recipe createRecipe(Recipe recipe) {
-        //add validation
         Optional<User> temp_user=userRepository.findById(recipe.getUser().getId());
         User user=temp_user.get();
         recipe.setUser(user);
@@ -41,5 +41,21 @@ public class RecipeService {
     public Collection<Recipe> getRecipesByUserIdAndRecipeName(Long id, String name){
         return recipeRepository.findByUserIdAndNameLikeIgnoreCaseOrderByNameAsc(id,"%"+name+"%");
 
+    }
+    public Recipe update(Recipe recipe){
+        Recipe oldRecipe=recipeRepository.findById(recipe.getId()).get();
+        oldRecipe.setName(recipe.getName());
+        oldRecipe.setImageUrl(recipe.getImageUrl());
+        oldRecipe.setServing(recipe.getServing());
+        oldRecipe.setLevel(recipe.getLevel());
+        oldRecipe.setRate(recipe.getRate());
+        oldRecipe.setTime(recipe.getTime());
+        oldRecipe.setVisibility(recipe.getVisibility());
+        oldRecipe.setUpdatedAt();
+        return recipeRepository.save(oldRecipe);
+    }
+
+    public void delete(Long id){
+        recipeRepository.deleteById(id);
     }
 }
