c04340a64268fe675b84f5933772f5b7378fbed6
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-16 02:03:30 +0200

    Recipe Repository Sorting Added to Queries

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/RecipeRepository.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/RecipeRepository.java
@@ -3,6 +3,8 @@ package com.example.butlerchef_backend.Repositories;
 import com.example.butlerchef_backend.Models.Recipe;
 import com.example.butlerchef_backend.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -11,6 +13,7 @@ import java.util.Optional;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
-    Collection<Recipe> findByNameLikeIgnoreCase(String name);
-    Collection<Recipe> findByUserId(Long id);
+    Collection<Recipe> findByNameLikeIgnoreCaseOrderByNameAsc(String name);
+    Collection<Recipe> findByUserIdAndNameLikeIgnoreCaseOrderByNameAsc(Long id, String name);
+    Collection<Recipe> findByUserIdOrderByCreatedAtDesc(Long id);
 }
