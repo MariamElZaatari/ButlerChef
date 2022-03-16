96af8fee17966d32f76cb6507b996d32e2929c1f
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-16 02:11:27 +0200

    User Repository Queries Added

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/UserRepository.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/UserRepository.java
@@ -5,13 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>{
 
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findUserByEmail(String email);
+    @Query("SELECT id, firstName, lastName, image FROM User u WHERE u.id = ?1")
+    List<Object[]> findUserById(Long id);
+    Collection<User> findUsersByFirstNameLikeIgnoreCaseOrLastNameLikeIgnoreCase(String firstName, String lastName);
+
+
 
 
 
