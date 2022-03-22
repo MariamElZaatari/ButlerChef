94900a086981273988bafbd476ba6d42df1b74f9
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-22 20:13:20 +0200

    Body Widget Added

---
+++ butlerchef_frontend/lib/widgets/body_widget.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:butler_chef/widgets/search_bar.dart';
+
+class BodyWidget extends StatelessWidget {
+  const BodyWidget({
+    Key? key,
+    required List<Widget> widgetOptions,
+    required int selectedIndex,
+  }) : _widgetOptions = widgetOptions, _selectedIndex = selectedIndex, super(key: key);
+
+  final List<Widget> _widgetOptions;
+  final int _selectedIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 30, 15, 15),
+      child: Column(
+//        crossAxisAlignment: CrossAxisAlignment.start,
+//        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SearchBar(),
+          Expanded(
+            flex: 9,
+            child: Center(
+              child: _widgetOptions.elementAt(_selectedIndex),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
