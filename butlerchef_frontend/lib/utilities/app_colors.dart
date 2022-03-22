7b5d9c3e780f1f2e8945eedce722136770626c58
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-22 20:07:40 +0200

    AppColors Added

---
+++ butlerchef_frontend/lib/utilities/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFFF2F2F2);
+  static const Color grayButton = Color(0xFFC8CBD2);
+  static const Color placeholder = Color(0xFFC6CACC);
+  static const Color green = Color(0xFF5ACE7F);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color brown = Color(0xFF6F5745);
+  static const Color red = Color(0xFFBF212F);
+  static const Color star = Color(0xFFFFE354);
+
+
+}
