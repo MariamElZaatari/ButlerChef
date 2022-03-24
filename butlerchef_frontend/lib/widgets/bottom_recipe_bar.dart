42acda95df9e70145bd960aace66ad9da8748cca
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-24 16:25:26 +0200

    BottomRecipeBar Widget Modified

--- butlerchef_frontend/lib/widgets/bottom_recipe_bar.dart
+++ butlerchef_frontend/lib/widgets/bottom_recipe_bar.dart
@@ -4,35 +4,54 @@ import 'package:butler_chef/widgets/recipe_info.dart';
 import 'package:butler_chef/widgets/recipe_author.dart';
 
 class BottomRecipeBar extends StatelessWidget {
+  final double _titleSize;
+  final double _infoSize;
+  final double _titleWidth;
+  final bool _isSmall;
   const BottomRecipeBar({
     Key? key,
-  }) : super(key: key);
+    required titleSize,
+    required infoSize,
+    required titleWidth,
+    required isSmall,
+  })  : _titleSize = titleSize,
+        _infoSize = infoSize,
+        _titleWidth = titleWidth,
+        _isSmall = isSmall,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const <Widget>[
-         Expanded(
+      children: <Widget>[
+        Expanded(
           flex: 7,
           child: Align(
             alignment: Alignment.topLeft,
-            child: RecipeTitle(),
+            child: RecipeTitle(
+              size: _titleSize,
+              width: _titleWidth,
+            ),
           ),
         ),
-         Expanded(
-          flex: 4,
+        _isSmall
+            ? Container()
+            : Expanded(
+                flex: 4,
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: RecipeInfo(
+                    size: _infoSize,
+                  ),
+                ),
+              ),
+        const Expanded(
+          flex: 2,
           child: Align(
-            alignment: Alignment.topLeft,
-            child: RecipeInfo(),
+            alignment: Alignment.bottomLeft,
+            child: RecipeAuthor(),
           ),
         ),
-         Expanded(
-           flex: 2,
-           child: Align(
-             alignment: Alignment.bottomLeft,
-             child: RecipeAuthor(),
-           ),
-         ),
       ],
     );
   }
