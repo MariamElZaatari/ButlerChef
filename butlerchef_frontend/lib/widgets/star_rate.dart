c2221eb040094c0fea994c15a2ec7bcd41780235
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-24 16:23:29 +0200

    StarRate Widget Modified

--- butlerchef_frontend/lib/widgets/star_rate.dart
+++ butlerchef_frontend/lib/widgets/star_rate.dart
@@ -2,39 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:butler_chef/utilities/app_colors.dart';
 
 class StarRate extends StatelessWidget {
+  final double _size;
+
   const StarRate({
-    Key? key,
-  }) : super(key: key);
+    Key? key, required double size,
+  }) : _size = size, super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
-      children: const <Widget>[
+      children: <Widget>[
         Icon(
           Icons.star_rounded,
           color: AppColors.star,
-          size: 28,
+          size: _size,
         ),
         Icon(
           Icons.star_rounded,
           color: AppColors.star,
-          size: 28,
+          size: _size,
         ),
         Icon(
           Icons.star_rounded,
           color: AppColors.star,
-          size: 28,
+          size: _size,
         ),
         Icon(
           Icons.star_rounded,
           color: AppColors.star,
-          size: 28,
+          size: _size,
         ),
         Icon(
           Icons.star_rounded,
           color: AppColors.star,
-          size: 28,
+          size: _size,
         ),
       ],
     );
