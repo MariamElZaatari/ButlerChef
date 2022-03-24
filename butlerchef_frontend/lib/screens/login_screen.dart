99943a6f61a61612487a08dca84f58f48959fa62
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-24 16:34:17 +0200

    Login Screen Modified

--- butlerchef_frontend/lib/screens/login_screen.dart
+++ butlerchef_frontend/lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:butler_chef/utilities/app_colors.dart';
 import 'package:butler_chef/screens/home_screen.dart';
+import 'package:butler_chef/widgets/input.dart';
+import 'package:butler_chef/screens/sign_up_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,75 +12,112 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.green,
       body: Center(
-        child: TextButton(
-          style: TextButton.styleFrom(
-            padding: const EdgeInsets.all(35),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            backgroundColor: AppColors.red,
-          ),
-          child: const Text(
-            "LOGIN",
-            style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.w900,
-                letterSpacing: 1.2,
-                color: AppColors.white),
-          ),
-          onPressed: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return const HomeScreen();
-                },
-              ),
-            )
-          },
-        ),
-      ),
+          child: Column(
+        children: [
+          const Expanded(
+              flex: 1,
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 81, 0, 0),
+                  child: Text(
+                    'ButlerChef',
+                    style: TextStyle(
+                        color: AppColors.white,
+                        fontSize: 38,
+                        fontWeight: FontWeight.w100,
+                        letterSpacing: 18),
+                  ))),
+          Expanded(
+              flex: 1,
+              child: Column(children: const [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 41, 0, 0),
+                  child: Text(
+                    'Welcome Back',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: AppColors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 2),
+                  ),
+                ),
+                Text(
+                  'You have been missed.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: AppColors.white,
+                      fontSize: 21,
+                      fontWeight: FontWeight.w300,
+                      letterSpacing: 2),
+                )
+              ])),
+          Expanded(
+              flex: 2,
+              child: Column(children: const [
+                Input(value: "Email"),
+                Input(value: "Password"),
+              ])),
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  SizedBox(
+                      width: 295,
+                      height: 89,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(21),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                          backgroundColor: AppColors.red,
+                        ),
+                        child: const Text(
+                          "Log In",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w900,
+                              letterSpacing: 2,
+                              color: AppColors.white),
+                        ),
+                        onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const HomeScreen();
+                              },
+                            ),
+                          )
+                        },
+                      )),
+                  SizedBox(
+                      width: 295,
+                      height: 41,
+                      child: TextButton(
+                        child: const Text(
+                          "Don't have an account?",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.white),
+                        ),
+                        onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const SignUpScreen();
+                              },
+                            ),
+                          )
+                        },
+                      )),
+                ],
+              ))
+        ],
+      )),
     );
   }
-//
-//  Widget loginButton() {
-//    return ;
-//  }
-
-//  void performLogin() {
-//    print("tring to perform login");
-//
-//    setState(() {
-//      isLoginPressed = true;
-//    });
-//
-//    _authMethods.signIn().then((User user) {
-//      if (user != null) {
-//        authenticateUser(user);
-//      } else {
-//        print("There was an error");
-//      }
-//    });
-//  }
-
-//  void authenticateUser(User user) {
-//    _authMethods.authenticateUser(user).then((isNewUser) {
-//      setState(() {
-//        isLoginPressed = false;
-//      });
-//
-//      if (isNewUser) {
-//        _authMethods.addDataToDb(user).then((value) {
-//          Navigator.pushReplacement(context,
-//              MaterialPageRoute(builder: (context) {
-//                return HomeScreen();
-//              }));
-//        });
-//      } else {
-//        Navigator.pushReplacement(context,
-//            MaterialPageRoute(builder: (context) {
-//              return HomeScreen();
-//            }));
-//      }
-//    });
-//  }
 }
