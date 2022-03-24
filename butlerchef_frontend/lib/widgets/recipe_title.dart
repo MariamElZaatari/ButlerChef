7786e84d4dc7f1d8606ca5954481fdd4d0027d07
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-24 16:25:10 +0200

    RecipeTitle Widget Modified

--- butlerchef_frontend/lib/widgets/recipe_title.dart
+++ butlerchef_frontend/lib/widgets/recipe_title.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:butler_chef/utilities/app_colors.dart';
 
 class RecipeTitle extends StatelessWidget {
+  final double _size;
+  final double _width;
   const RecipeTitle({
     Key? key,
-  }) : super(key: key);
+    required size, required width
+  })  : _size = size, _width=width,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-        width: 400,
+    return SizedBox(
+        width: _width,
         child: Text(
           "Hot Shakshuka with Eggs",
           style: TextStyle(
-            fontSize: 36,
+            fontSize: _size,
             fontWeight: FontWeight.w900,
             letterSpacing: 4,
             color: AppColors.white,
