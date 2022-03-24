9da39638803c38a21ffd18a80fb7f1b0b776b9d0
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-24 16:23:56 +0200

    TopRecipeBar Widget Modified

--- butlerchef_frontend/lib/widgets/top_recipe_bar.dart
+++ butlerchef_frontend/lib/widgets/top_recipe_bar.dart
@@ -3,21 +3,22 @@ import 'package:butler_chef/widgets/star_rate.dart';
 import 'package:butler_chef/widgets/ingredient_stats.dart';
 
 class TopRecipeBar extends StatelessWidget {
+  final double _starSize;
   const TopRecipeBar({
-    Key? key,
-  }) : super(key: key);
+    Key? key, required starSize
+  }) : _starSize=starSize, super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const <Widget>[
+      children: <Widget>[
         Expanded(
           child: Align(
             alignment: Alignment.centerLeft,
-            child: StarRate(),
+            child: StarRate(size: _starSize,),
           ),
         ),
-        Expanded(
+        const Expanded(
           child: Align(
             alignment: Alignment.centerRight,
             child: IngredientStats(),
