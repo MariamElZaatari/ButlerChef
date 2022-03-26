5938689bcc519c3f07e6c2e509a7245db0208c47
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-26 14:56:44 +0200

    App Wrapped with MultiProviders, including UserProvider and TokenProvider

--- butlerchef_frontend/lib/main.dart
+++ butlerchef_frontend/lib/main.dart
@@ -1,10 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:butler_chef/screens/login_screen.dart';
+import 'package:butler_chef/providers/token_provider.dart';
+import 'package:butler_chef/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 
-void main() => runApp(const MyApp());
-
+void main() => runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => TokenProvider()),
+      ],
+      child: MyApp(),
+    )
+);
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
