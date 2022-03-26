9e79c8c647736432aac4bd90ebdbd164f0272160
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-26 14:55:08 +0200

    AppColors Utility Modified with darkGreen and grayIcon

--- butlerchef_frontend/lib/utils/app_colors.dart
+++ butlerchef_frontend/lib/utils/app_colors.dart
@@ -5,7 +5,10 @@ class AppColors {
   static const Color grayButton = Color(0xFFC8CBD2);
   static const Color placeholder = Color(0xFFC6CACC);
   static const Color green = Color(0xFF5ACE7F);
+  static const Color darkGreen = Color(0xFF14281D);
+
   static const Color white = Color(0xFFFFFFFF);
+  static const Color grayIcon = Color(0xFF8F9195);
   static const Color black = Color(0xFF000000);
   static const Color brown = Color(0xFF6F5745);
   static const Color red = Color(0xFFBF212F);
