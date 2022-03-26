81a6a6b0901629a30877d71d702254f5bbfaf912
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-26 14:53:46 +0200

    AuthService Added

---
+++ butlerchef_frontend/lib/services/auth_service.dart
@@ -0,0 +1,93 @@
+import 'package:butler_chef/models/user_model.dart';
+import 'package:butler_chef/utils/app_url.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class AuthService {
+
+  static Future<Map<String, dynamic>> login(String email, String password) async {
+    Map<String, Object> result;
+
+    final Map<String, dynamic> loginData = {
+      'email': email,
+      'password': password
+    };
+
+    Response response = await post(
+      Uri.parse(AppUrl.login),
+      body: json.encode(loginData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+
+      print(responseData);
+
+      var userData = responseData['user'];
+      var token = responseData['token'];
+
+      User authUser = User.fromJson(userData);
+
+      result = {'status': true, 'message': 'Successful', 'user': authUser, 'token': token};
+    }
+    else {
+      result = {'status': false, 'message': 'Failed'};
+    }
+
+    return result;
+  }
+
+  static Future<Map<String, dynamic>> register(
+      String email,
+      String password,
+      String repeatPassword,
+      String firstName,
+      String lastName,
+      ) async {
+
+    Map<String, Object> result;
+
+    // if the password does not match, return failed with status false and a message.
+    if(password != repeatPassword){
+      return {'status': false, 'message': 'Password does not match'};
+    }
+
+    // fill the data inside am object to be send to the backend.
+    final Map<String, dynamic> loginData = {
+      'email': email,
+      'password': password,
+      'firstName': firstName,
+      'lastName': lastName,
+      'imageUrl': "",
+      "userRole": {"id":2}
+    };
+
+    // send the loginData obj to the register api.
+    Response response = await post(
+      Uri.parse(AppUrl.register),
+      body: json.encode(loginData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    // if the response came successfully.
+    if (response.statusCode == 200) {
+      // decode the response to a Map of key: String, value: Object.
+      final Map<String, dynamic> responseData = json.decode(response.body);
+
+      print(responseData);
+
+      result = { 'status': true, 'message': responseData['message'] };
+    }
+    else {
+      result = { 'status': false, 'message': 'Failed' };
+    }
+
+    return result;
+  }
+
+}
