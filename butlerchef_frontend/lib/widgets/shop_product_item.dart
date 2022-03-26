4ded2786822b160ea6291c6f967f578b0061cc93
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-26 15:06:30 +0200

    Refactored utilities to utils and quantity type to double, and Removed widget container wrapper

--- butlerchef_frontend/lib/widgets/shop_product_item.dart
+++ butlerchef_frontend/lib/widgets/shop_product_item.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:butler_chef/utilities/app_colors.dart';
+import 'package:butler_chef/utils/app_colors.dart';
 
 // ProductWidget
 
 class ShopProductItem extends StatelessWidget {
   final String productName;
   final String image;
-  final double quantity;
+  final String quantity;
   final String measurement;
   final double price;
 
@@ -21,91 +21,100 @@ class ShopProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 221,
-      width: 200,
-      margin: const EdgeInsets.fromLTRB(0, 0, 21, 0),
-      padding: const EdgeInsets.fromLTRB(18, 2, 18, 25),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 141,
-                width: 200,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          'https://thumbs.dreamstime.com/b/fresh-tomato-isolated-white-background-close-up-fresh-tomato-isolated-white-background-close-up-124753596.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(20))),
-              ),
-              Container(
-                  height: 141,
-                  width: 200,
-                  decoration: const BoxDecoration(
-                    borderRadius:
-                    BorderRadius.vertical(top: Radius.circular(20)),
-                    gradient: LinearGradient(
-                      begin: Alignment.center,
-                      end: Alignment
-                          .bottomCenter, // 10% of the width, so there are ten blinds.
-                      colors: <Color>[
-                        Color(0x00000000),
-                        Color(0x66000000),
-                      ], // red to yellow // repeats the gradient over the canvas
-                    ),
-                  )),
-              const SizedBox(
-                height: 141,
-                width: 200,
-                child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Text(
-                      'Tomatoes',
-                      style: TextStyle(
-                          fontSize: 21,
-                          fontWeight: FontWeight.w400,
-                          color: AppColors.white),
-                    )),
-              ),
-            ],
-          ),
-          Container(
-              height: 28,
-              width: 200,
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: 141,
+              width: 171,
               decoration: const BoxDecoration(
-                borderRadius:
-                BorderRadius.vertical(bottom: Radius.circular(20)),
-                gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment
-                      .centerRight,
-                  stops: [0, 0.5, 0.5, 1.0],
-                  colors: <Color>[
-                    AppColors.white,
-                    AppColors.white,
-                    AppColors.brown,
-                    AppColors.brown,
-                  ],
-                ),
-              ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Text('500G', style: TextStyle(fontWeight: FontWeight.w900, color: AppColors.brown, fontSize: 12),),
-                Text("\$2.00", style: TextStyle(fontWeight: FontWeight.w900, color: AppColors.white, fontSize: 12),),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://thumbs.dreamstime.com/b/fresh-tomato-isolated-white-background-close-up-fresh-tomato-isolated-white-background-close-up-124753596.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(5))),
+            ),
+            Container(
+                height: 141,
+                width: 171,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
+                  gradient: LinearGradient(
+                    begin: Alignment.center,
+                    end: Alignment
+                        .bottomCenter, // 10% of the width, so there are ten blinds.
+                    colors: <Color>[
+                      Color(0x00000000),
+                      Color(0x66000000),
+                    ], // red to yellow // repeats the gradient over the canvas
+                  ),
+                )),
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+              height: 141,
+              width: 171,
+              child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    productName,
+                    style: const TextStyle(
+                        fontSize: 21,
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.white),
+                  )),
+            ),
+          ],
+        ),
+        Container(
+          height: 28,
+          width: 171,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(5)),
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              stops: [0, 0.5, 0.5, 1.0],
+              colors: <Color>[
+                AppColors.white,
+                AppColors.white,
+                AppColors.brown,
+                AppColors.brown,
               ],
             ),
           ),
-        ],
-      ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      quantity + measurement,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w900,
+                          color: AppColors.brown,
+                          fontSize: 12),
+                    )),
+                flex: 5,
+              ),
+              Expanded(
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "\$" + price.toString(),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w900,
+                          color: AppColors.white,
+                          fontSize: 12),
+                    )),
+                flex: 5,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
