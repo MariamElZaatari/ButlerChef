e38e17d06bebddc27be4ec2e26929bc4398b4ec5
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-26 15:07:44 +0200

    Functioning ShopProductItem with Linking Added to Shop Widget

--- butlerchef_frontend/lib/widgets/shop.dart
+++ butlerchef_frontend/lib/widgets/shop.dart
@@ -1,49 +1,149 @@
 import 'dart:convert';
-
 import 'package:butler_chef/models/shop_product_model.dart';
+import 'package:butler_chef/widgets/shop_product_item.dart';
 import 'package:flutter/material.dart';
+import 'package:butler_chef/utils/app_colors.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:butler_chef/screens/cart_screen.dart';
 import 'package:http/http.dart' as http;
 
 class Shop extends StatefulWidget {
-
   const Shop({Key? key}) : super(key: key);
 
   @override
   ShopState createState() => ShopState();
 }
 
-Future<List<ShopProductModel>> fetchShopProducts () async {
-  final response = await http
-      .get(Uri.parse('http://10.0.2.2:8080/api/shopProduct/'));
+Future<List<ShopProductModel>> fetchShopProducts() async {
+  final response =
+      await http.get(Uri.parse('http://10.0.2.2:8080/api/shopProduct/'));
 
 //  print(response.body);
   if (response.statusCode == 200) {
-    ShopProductResponse tempRes = ShopProductResponse.fromJson(jsonDecode(response.body));
+    ShopProductResponse tempRes =
+        ShopProductResponse.fromJson(jsonDecode(response.body));
     return tempRes.data;
   } else {
     throw Exception('Failed to load album');
   }
-
 }
 
 class ShopState extends State<Shop> {
   late List<ShopProductModel> shopProducts = <ShopProductModel>[];
+  late List<ShopProductModel> selectedProducts = <ShopProductModel>[];
+  late List<bool> selected = <bool>[];
 
   @override
   void initState() {
-    super.initState();
-    fetchShopProducts().then((data) {
-      setState(() {
-        shopProducts = data;
+    Future.delayed(Duration.zero, () async {
+      await fetchShopProducts().then((data) {
+        setState(() {
+          shopProducts = data;
+        });
       });
+      for (var i = 0; i < shopProducts.length; i++) {
+        selected.add(false);
+      }
     });
+    super.initState();
   }
+
+  Icon notSelectedIcon =
+      const Icon(Icons.add_circle, color: AppColors.grayIcon, size: 35);
+  Icon selectedIcon =
+      const Icon(Icons.check_circle, color: AppColors.green, size: 35);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-        children: shopProducts.map((item) => Text(item.name)).toList()
-    );
-  }
+    List<ShopProductItem> shopProductItems = shopProducts
+        .map((item) => ShopProductItem(
+            productName: item.name,
+            image: item.imageUrl,
+            quantity: item.quantity.value,
+            measurement: item.measurement.value,
+            price: item.price))
+        .toList();
 
+    return Center(
+        child: Stack(
+      children: [
+        GridView.builder(
+            padding: const EdgeInsets.fromLTRB(18, 10, 0, 21),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                crossAxisSpacing: 1,
+                mainAxisSpacing: 5),
+            itemCount: shopProductItems.length,
+            itemBuilder: (BuildContext ctx, index) {
+              return Center(
+                  child: Stack(
+                children: [
+                  shopProductItems[index],
+                  Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                          icon: selected.elementAt(index)
+                              ? selectedIcon
+                              : notSelectedIcon,
+                          onPressed: () => {
+                                setState(() => {
+                                      selected[index] =
+                                          !selected.elementAt(index)
+                                    })
+                              })),
+                ],
+              ));
+            }),
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+                width: 341,
+                height: 41,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0))),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(AppColors.green),
+                      overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.focused) ||
+                              states.contains(MaterialState.pressed)) {
+                            return AppColors.white.withOpacity(0.12);
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+//                        MaterialPageRoute(
+//                            builder: (context) => CartScreen(
+//                                  selectedProducts: selectedProducts,
+//                                )),
+                        MaterialPageRoute(builder: (context) => const CartScreen()),
+                      );
+                    },
+                    child: RichText(
+                      text: const TextSpan(
+                        children: [
+                          WidgetSpan(
+                            child: FaIcon(FontAwesomeIcons.shoppingBasket,
+                                color: AppColors.white, size: 21),
+                          ),
+                          TextSpan(
+                              text: " View Basket",
+                              style: TextStyle(
+                                  fontSize: 21,
+                                  fontWeight: FontWeight.w300,
+                                  color: AppColors.white,
+                                  letterSpacing: 4)),
+                        ],
+                      ),
+                    ))))
+      ],
+    ));
+  }
 }
