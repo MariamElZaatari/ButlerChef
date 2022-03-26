9336ec3a784d7824902db7b2318469c8e6437559
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-26 14:54:21 +0200

    AppUrl Utility Added

---
+++ butlerchef_frontend/lib/utils/app_url.dart
@@ -0,0 +1,8 @@
+class AppUrl {
+
+  static const String baseUrl = 'http://10.0.2.2:8080/api';
+
+  static const String login = baseUrl + '/auth/login';
+  static const String register = baseUrl + '/auth/register';
+
+}
