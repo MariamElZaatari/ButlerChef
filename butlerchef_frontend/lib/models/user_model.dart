2e1d406bcf16e189e2f42ee72a97fa562404c9d2
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-27 08:01:16 +0300

    User Model Modified

--- butlerchef_frontend/lib/models/user_model.dart
+++ butlerchef_frontend/lib/models/user_model.dart
@@ -1,27 +1,20 @@
-import 'package:butler_chef/models/user_role_model.dart';
-
 class User {
   User({
     required this.id,
-    required this.email,
     // required this.password,
     required this.firstName,
     required this.lastName,
     required this.imageUrl,
-    required this.userRole,
+    this.email = "",
     // required this.createdAt,
     // required this.updatedAt,
   });
 
   int id;
   String email;
-  // String password;
   String firstName;
   String lastName;
   String imageUrl;
-  UserRole userRole;
-  // DateTime createdAt;
-  // DateTime updatedAt;
 
   factory User.fromJson(Map<String, dynamic> json) => User(
     id: json["id"],
@@ -30,20 +23,13 @@ class User {
     firstName: json["firstName"],
     lastName: json["lastName"],
     imageUrl: json["imageUrl"],
-    userRole: UserRole.fromJson(json["userRole"]),
-    // createdAt: DateTime.parse(json["created_at"]),
-    // updatedAt: DateTime.parse(json["updated_at"]),
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
     "email": email,
-    // "password": password,
     "firstName": firstName,
     "lastName": lastName,
     "imageUrl": imageUrl,
-    "userRole": userRole.toJson(),
-    // "created_at": createdAt.toIso8601String(),
-    // "updated_at": updatedAt.toIso8601String(),
   };
 }
