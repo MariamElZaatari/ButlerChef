1c04cd7eebf53eaf7dc465d042914b3d30290e12
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-27 08:09:23 +0300

    CustomBackButton Added

---
+++ butlerchef_frontend/lib/widgets/custom_back_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:butler_chef/utils/app_colors.dart';
+
+class CustomBackButton extends StatelessWidget {
+  const CustomBackButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 45,
+        width: 45,
+        margin: const EdgeInsets.only(left: 5, top: 5),
+        decoration: BoxDecoration(
+          color: AppColors.green,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: InkWell(
+            onTap: () => {Navigator.pop(context)},
+            child: const Align(
+                alignment: Alignment.center,
+                child: Icon(Icons.arrow_back_ios_new_rounded,
+                    color: AppColors.white, size: 41))));
+  }
+}
