30485e10d53ec69e609cf8b7deb3a7012ce0e1a7
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-28 00:15:36 +0300

    getFavoriteRecipesInfoForLoggedUser Added to RecipeRepository

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/RecipeRepository.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/RecipeRepository.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.Optional;
 
 @Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long>{
 
     Collection<Recipe> findByNameLikeIgnoreCaseOrderByNameAsc(String name);
     Collection<Recipe> findByUserIdAndNameLikeIgnoreCaseOrderByNameAsc(Long id, String name);
@@ -19,4 +19,7 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     @Query("SELECT r as recipe, (SELECT COUNT(*) FROM RecipeProduct rp WHERE rp.recipe.id=r.id) as total,(SELECT COUNT(*) FROM FridgeProduct fp WHERE fp.user.id=?1 AND fp.name IN (SELECT name FROM RecipeProduct rp WHERE rp.recipe.id=r.id)) as fridge, (SELECT COUNT(*) FROM FavoriteRecipe fr WHERE fr.user.id=?1 AND fr.recipe.id=r.id) as favorite, (SELECT COUNT(*) FROM CookedRecipe cr WHERE cr.user.id=?1 AND cr.recipe.id=r.id) as cooked FROM Recipe r GROUP BY r.id")
     Collection<RecipeDisplayInfo> getRecipesInfoForLoggedUser(Long id);
+
+    @Query("SELECT r as recipe, (SELECT COUNT(*) FROM RecipeProduct rp WHERE rp.recipe.id=r.id) as total,(SELECT COUNT(*) FROM FridgeProduct fp WHERE fp.user.id=?1 AND fp.name IN (SELECT name FROM RecipeProduct rp WHERE rp.recipe.id=r.id)) as fridge,(SELECT COUNT(*) FROM CookedRecipe cr WHERE cr.user.id=?1 AND cr.recipe.id=r.id) as cooked FROM Recipe r GROUP BY r.id HAVING (SELECT COUNT(*) FROM FavoriteRecipe fr WHERE fr.user.id=?1 AND fr.recipe.id=r.id)=1")
+    Collection<FavoriteRecipeDisplayInfo> getFavoriteRecipesInfoForLoggedUser(Long id);
 }
