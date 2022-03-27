bf803816f8fc1121989c0cda7e137729c37bd8e2
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-27 08:03:23 +0300

    Home Navigation Bar Icons Modified

--- butlerchef_frontend/lib/screens/home_screen.dart
+++ butlerchef_frontend/lib/screens/home_screen.dart
@@ -1,12 +1,10 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:butler_chef/widgets/body_widget.dart';
 import 'package:butler_chef/widgets/large_recipe.dart';
 import 'package:butler_chef/utils/app_colors.dart';
 import 'package:butler_chef/widgets/profile.dart';
 import 'package:butler_chef/widgets/fridge.dart';
-import 'package:butler_chef/widgets/shop_product_item.dart';
 import 'package:butler_chef/widgets/shop.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -44,37 +42,45 @@ class HomeScreenState extends State<HomeScreen> {
       resizeToAvoidBottomInset: false,
       body: BodyWidget(
           widgetOptions: _widgetOptions, selectedIndex: _selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.fileAlt, size: 28),
-            label: 'Recipe',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart_outlined, size: 35),
-            label: 'Shop',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline, size: 50),
-            label: 'Post',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.kitchen_outlined, size: 35),
-            label: 'Fridge',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.user, size: 28),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        unselectedItemColor: AppColors.grayButton,
-        selectedItemColor: AppColors.green,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        iconSize: 30,
+      bottomNavigationBar: SizedBox(
+        height: 75,
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.description_outlined, size: 35),
+              activeIcon: Icon(Icons.description_rounded, size: 35),
+              label: 'Recipe',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_grocery_store_outlined , size: 35),
+              activeIcon: Icon(Icons.local_grocery_store_rounded , size: 35),
+              label: 'Shop',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline, size: 45),
+              activeIcon: Icon(Icons.add_circle_rounded, size: 45),
+              label: 'Post',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.kitchen_outlined, size: 35),
+              activeIcon: Icon(Icons.kitchen_rounded, size: 35),
+              label: 'Fridge',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_rounded, size: 35),
+              activeIcon: Icon(Icons.person_rounded, size: 35),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          unselectedItemColor: AppColors.grayButton,
+          selectedItemColor: AppColors.green,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          iconSize: 30,
+        ),
       ),
       backgroundColor: AppColors.backgroundColor,
     );
