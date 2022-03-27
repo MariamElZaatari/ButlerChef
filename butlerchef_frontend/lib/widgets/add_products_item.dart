d4dfe4d4c6f3959322c5df085a73e49647447f4c
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-27 08:17:07 +0300

    AddProductsItem animation and callback attributes added

--- butlerchef_frontend/lib/widgets/add_products_item.dart
+++ butlerchef_frontend/lib/widgets/add_products_item.dart
@@ -5,123 +5,151 @@ import 'package:butler_chef/utils/app_colors.dart';
 // ProductWidget
 
 class AddProductsItem extends StatefulWidget {
-//  final String productName;
-//  final String quantity;
-//  final String measurement;
+//  final Product item;
+  final Animation<double> _animation;
+  final VoidCallback _onClicked;
 
-  const AddProductsItem({
-    Key? key,
-//    required this.productName,
-//    required this.quantity,
-//    required this.measurement,
-  }) : super(key: key);
+  const AddProductsItem({Key? key, required animation, required onClicked})
+      : _animation=animation, _onClicked=onClicked, super(key: key);
 
   @override
   AddProductsItemState createState() => AddProductsItemState();
 }
+
 class AddProductsItemState extends State<AddProductsItem> {
   String dropdownQuantityValue = '1';
   String dropdownMeasurementValue = 'kg';
 
+
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        margin: const EdgeInsets.symmetric(
-          horizontal: 10,
-        ),
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const Align(
-                    alignment: Alignment.topLeft,
-                    child: TextField(
-                      textAlign: TextAlign.left,
-                      style: ThemeText.productName,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Product...",
-                          hintStyle: TextStyle(
-                              color: AppColors.placeholder,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20)),
-                    )),
-                const SizedBox(height: 4),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    TextButton(
-                        onPressed: () => {},
-                        style: TextButton.styleFrom(
-                            padding: EdgeInsets.zero,
-                            alignment: Alignment.centerLeft),
-                        child: const Text(
-                          "Remove",
-                          style: TextStyle(
-                              color: AppColors.red,
-                              fontWeight: FontWeight.w900,
-                              fontSize: 19),
-                        )),
-                    Row(
-                      children: [
-                        PopupMenuButton<String>(
-                          itemBuilder: (context){
-                            return <String>['1', '2', '3', '4']
-                                .map<PopupMenuItem<String>>((String value) {
-                              return PopupMenuItem<String>(
-                                value: value,
-                                child: Text(value, style: ThemeText.quantityMeasurement,),
-                              );
-                            }).toList();
-                          },
-                          child: Row(
-                            children: [
-                              Text(dropdownQuantityValue, style: ThemeText.quantityMeasurement,),
-                              const Icon(Icons.keyboard_arrow_down_rounded, color: AppColors.green, size: 24,),
-                            ],
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(-1, 0),
+        end: Offset(0, 0),
+      ).animate(CurvedAnimation(
+          parent: widget._animation,
+          curve: Curves.bounceIn,
+          reverseCurve: Curves.bounceOut)),
+//      sizeFactor: widget._animation,
+      child: Center(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 10, vertical: 10
+          ),
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Align(
+                      alignment: Alignment.topLeft,
+                      child: TextField(
+//                        key: ValueKey(1),
+                        textAlign: TextAlign.left,
+                        style: ThemeText.productName,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Product...",
+                            hintStyle: TextStyle(
+                                color: AppColors.placeholder,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20)),
+                      )),
+                  const SizedBox(height: 4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      TextButton(
+                          onPressed: widget._onClicked,
+                          style: TextButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              alignment: Alignment.centerLeft),
+                          child: const Text(
+                            "Remove",
+                            style: TextStyle(
+                                color: AppColors.red,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 19),
+                          )),
+                      Row(
+                        children: [
+                          PopupMenuButton<String>(
+                            itemBuilder: (context) {
+                              return <String>['1', '2', '3', '4']
+                                  .map<PopupMenuItem<String>>((String value) {
+                                return PopupMenuItem<String>(
+                                  value: value,
+                                  child: Text(
+                                    value,
+                                    style: ThemeText.quantityMeasurement,
+                                  ),
+                                );
+                              }).toList();
+                            },
+                            child: Row(
+                              children: [
+                                Text(
+                                  dropdownQuantityValue,
+                                  style: ThemeText.quantityMeasurement,
+                                ),
+                                const Icon(
+                                  Icons.keyboard_arrow_down_rounded,
+                                  color: AppColors.green,
+                                  size: 24,
+                                ),
+                              ],
+                            ),
+                            onSelected: (String? newValue) {
+                              setState(() {
+                                dropdownQuantityValue = newValue!;
+                              });
+                            },
                           ),
-                          onSelected: (String? newValue) {
-                        setState(() {
-                        dropdownQuantityValue = newValue!;
-                        });
-                        },
-
-                        ),
-                        const SizedBox(width: 12),
-                        PopupMenuButton<String>(
-                          itemBuilder: (context){
-                            return <String>['kg', 'dz', 'g', 'can']
-                                .map<PopupMenuItem<String>>((String value) {
-                              return PopupMenuItem<String>(
-                                value: value,
-                                child: Text(value, style: ThemeText.quantityMeasurement,),
-                              );
-                            }).toList();
-                          },
-                          child: Row(
-                            children: [
-                            Text(dropdownMeasurementValue, style: ThemeText.quantityMeasurement,),
-                            const Icon(Icons.keyboard_arrow_down_rounded, color: AppColors.green, size: 24,),
-                          ],
+                          const SizedBox(width: 12),
+                          PopupMenuButton<String>(
+                            itemBuilder: (context) {
+                              return <String>['kg', 'dz', 'g', 'can']
+                                  .map<PopupMenuItem<String>>((String value) {
+                                return PopupMenuItem<String>(
+                                  value: value,
+                                  child: Text(
+                                    value,
+                                    style: ThemeText.quantityMeasurement,
+                                  ),
+                                );
+                              }).toList();
+                            },
+                            child: Row(
+                              children: [
+                                Text(
+                                  dropdownMeasurementValue,
+                                  style: ThemeText.quantityMeasurement,
+                                ),
+                                const Icon(
+                                  Icons.keyboard_arrow_down_rounded,
+                                  color: AppColors.green,
+                                  size: 24,
+                                ),
+                              ],
+                            ),
+                            onSelected: (String? newValue) {
+                              setState(() {
+                                dropdownMeasurementValue = newValue!;
+                              });
+                            },
                           ),
-                          onSelected: (String? newValue) {
-                        setState(() {
-                        dropdownMeasurementValue = newValue!;
-                        });
-                        },
-
-                        ),
-                      ],
-                    )
-                  ],
-                )
-              ],
-            )),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
