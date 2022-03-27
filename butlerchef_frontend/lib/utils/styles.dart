98be9d2f923123bbb41bd050fc2a893133caa570
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-27 08:07:41 +0300

    directionTitle and directionContent TextStyles Added

--- butlerchef_frontend/lib/utils/styles.dart
+++ butlerchef_frontend/lib/utils/styles.dart
@@ -19,4 +19,10 @@ class ThemeText {
 
   static const TextStyle quantityMeasurement = TextStyle(
       fontWeight: FontWeight.normal, fontSize: 20, color: AppColors.green);
+
+  static const TextStyle directionTitle = TextStyle(
+      fontWeight: FontWeight.w700, fontSize: 30, color: AppColors.green, letterSpacing: 2);
+
+  static const TextStyle directionContent = TextStyle(
+      fontWeight: FontWeight.w300, fontSize: 20, color: AppColors.brown, letterSpacing: 1);
 }
