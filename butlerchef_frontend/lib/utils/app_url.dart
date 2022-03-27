1c5bb0ce222effc1ca622087c47db2490d99701e
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-27 08:04:16 +0300

    RecipesByUserId URL Added

--- butlerchef_frontend/lib/utils/app_url.dart
+++ butlerchef_frontend/lib/utils/app_url.dart
@@ -4,5 +4,7 @@ class AppUrl {
 
   static const String login = baseUrl + '/auth/login';
   static const String register = baseUrl + '/auth/register';
+  static const String recipesByUserId = baseUrl + '/recipe/';
+
 
 }
