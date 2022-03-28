9b20fff8462bcf3614f1bd484297447045282634
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-28 18:44:03 +0300

    getProfileRecipesInfoForLoggedUser Added to RecipeRepository

--- butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/RecipeRepository.java
+++ butlerchef_backend/src/main/java/com/example/butlerchef_backend/Repositories/RecipeRepository.java
@@ -25,4 +25,7 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long>{
 
     @Query("SELECT r as recipe, (SELECT COUNT(*) FROM RecipeProduct rp WHERE rp.recipe.id=r.id) as total,(SELECT COUNT(*) FROM FridgeProduct fp WHERE fp.user.id=?1 AND fp.name IN (SELECT name FROM RecipeProduct rp WHERE rp.recipe.id=r.id)) as fridge,(SELECT COUNT(*) FROM FavoriteRecipe fr WHERE fr.user.id=?1 AND fr.recipe.id=r.id) as favorite FROM Recipe r GROUP BY r.id HAVING (SELECT COUNT(*) FROM CookedRecipe cr WHERE cr.user.id=?1 AND cr.recipe.id=r.id)=1")
     Collection<CookedRecipeDisplayInfo> getCookedRecipesInfoForLoggedUser(Long id);
+
+    @Query("SELECT r as recipe, (SELECT COUNT(*) FROM RecipeProduct rp WHERE rp.recipe.id=r.id) as total,(SELECT COUNT(*) FROM FridgeProduct fp WHERE fp.user.id=?1 AND fp.name IN (SELECT name FROM RecipeProduct rp WHERE rp.recipe.id=r.id)) as fridge, (SELECT COUNT(*) FROM FavoriteRecipe fr WHERE fr.user.id=?1 AND fr.recipe.id=r.id) as favorite, (SELECT COUNT(*) FROM CookedRecipe cr WHERE cr.user.id=?1 AND cr.recipe.id=r.id) as cooked FROM Recipe r WHERE r.user.id=?1 GROUP BY r.id")
+    Collection<RecipeDisplayInfo> getProfileRecipesInfoForLoggedUser(Long id);
 }
