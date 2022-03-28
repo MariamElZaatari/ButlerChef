7c0880e59585bcd07896b1bf9e26ad5eeff78fcf
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-28 17:48:08 +0300

    EditButton Widget Reformatted Code

--- butlerchef_frontend/lib/widgets/edit_button.dart
+++ butlerchef_frontend/lib/widgets/edit_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:butler_chef/utils/app_colors.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../screens/edit_profile_screen.dart';
 
 class EditButton extends StatelessWidget {
   const EditButton({
@@ -10,24 +11,24 @@ class EditButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Align(
-        alignment: Alignment.bottomRight,
-        child: IconButton(
-          icon: const FaIcon(
-            FontAwesomeIcons.solidEdit,
-            size: 25,
-            color: AppColors.green,
-          ),
-          onPressed: () => {
-//                        Navigator.push(
-//                          context,
-//                          MaterialPageRoute(
-//                            builder: (context) {
-//                              return EditProfileScreen();
-//                            },
-//                          ),
-//                        )
-          },
+      alignment: Alignment.bottomRight,
+      child: IconButton(
+        icon: const FaIcon(
+          FontAwesomeIcons.solidEdit,
+          size: 25,
+          color: AppColors.green,
         ),
+        onPressed: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditProfileScreen();
+            },
+          ),
+        )
+        },
+      ),
     );
   }
 }
