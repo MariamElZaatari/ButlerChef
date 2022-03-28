3825533a3806bbac910e81569468cdaf069fa54a
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-28 16:34:19 +0300

    UserModel Modified

--- butlerchef_frontend/lib/models/user_model.dart
+++ butlerchef_frontend/lib/models/user_model.dart
@@ -1,25 +1,21 @@
 class User {
   User({
-    required this.id,
-    // required this.password,
-    required this.firstName,
-    required this.lastName,
-    required this.imageUrl,
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.imageUrl,
     this.email = "",
-    // required this.createdAt,
-    // required this.updatedAt,
   });
 
-  int id;
-  String email;
-  String firstName;
-  String lastName;
-  String imageUrl;
+  int? id;
+  String? email;
+  String? firstName;
+  String? lastName;
+  String? imageUrl;
 
   factory User.fromJson(Map<String, dynamic> json) => User(
     id: json["id"],
     email: json["email"],
-    // password: json["password"],
     firstName: json["firstName"],
     lastName: json["lastName"],
     imageUrl: json["imageUrl"],
