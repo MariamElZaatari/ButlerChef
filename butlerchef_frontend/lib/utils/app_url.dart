6a5730b485f8bcd305c00f256511486264580d58
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-28 17:42:20 +0300

    Added createRecipes, favoriteRecipesByUserId, cookedRecipesByUserId, fridgeProductByUserId, createFridgeProduct url to appUrl

--- butlerchef_frontend/lib/utils/app_url.dart
+++ butlerchef_frontend/lib/utils/app_url.dart
@@ -4,7 +4,13 @@ class AppUrl {
 
   static const String login = baseUrl + '/auth/login';
   static const String register = baseUrl + '/auth/register';
+
   static const String recipesByUserId = baseUrl + '/recipe/';
+  static const String createRecipes = baseUrl + '/recipe/create';
+  static const String favoriteRecipesByUserId = baseUrl + '/favorite/read/';
+  static const String cookedRecipesByUserId = baseUrl + '/cooked/read/';
+  static const String fridgeProductByUserId = baseUrl + '/fridgeProduct/';
+  static const String createFridgeProduct = baseUrl + '/fridgeProduct/create/';
 
 
 }
