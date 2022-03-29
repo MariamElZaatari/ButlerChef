dee7f143aac258afb7749e1ec27b58a4242eb9f2
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-29 08:49:54 +0300

    BottomRecipeBar Widget Linked to Backend

--- butlerchef_frontend/lib/widgets/bottom_recipe_bar.dart
+++ butlerchef_frontend/lib/widgets/bottom_recipe_bar.dart
@@ -3,17 +3,31 @@ import 'package:butler_chef/widgets/recipe_title.dart';
 import 'package:butler_chef/widgets/recipe_info.dart';
 import 'package:butler_chef/widgets/recipe_author.dart';
 
+import '../models/user_model.dart';
+
 class BottomRecipeBar extends StatelessWidget {
   final double _titleSize;
   final double _infoSize;
   final double _titleWidth;
   final bool _isSmall;
+  final String? name;
+  final String? level;
+  final String? time;
+  final User? user;
+  final String? serving;
+
+
   const BottomRecipeBar({
     Key? key,
     required titleSize,
     required infoSize,
     required titleWidth,
     required isSmall,
+    required this.name,
+    required this.level,
+    required this.time,
+    required this.user,
+    required this.serving,
   })  : _titleSize = titleSize,
         _infoSize = infoSize,
         _titleWidth = titleWidth,
@@ -25,12 +39,13 @@ class BottomRecipeBar extends StatelessWidget {
     return Column(
       children: <Widget>[
         Expanded(
-          flex: 7,
+          flex: 8,
           child: Align(
-            alignment: Alignment.topLeft,
+            alignment: Alignment.bottomLeft,
             child: RecipeTitle(
               size: _titleSize,
               width: _titleWidth,
+              title: name,
             ),
           ),
         ),
@@ -42,14 +57,19 @@ class BottomRecipeBar extends StatelessWidget {
                   alignment: Alignment.topLeft,
                   child: RecipeInfo(
                     size: _infoSize,
+                    level: level,
+                    serving: serving,
+                    time: time,
                   ),
                 ),
               ),
-        const Expanded(
+        Expanded(
           flex: 2,
           child: Align(
             alignment: Alignment.bottomLeft,
-            child: RecipeAuthor(),
+            child: RecipeAuthor(
+              user: user,
+            ),
           ),
         ),
       ],
