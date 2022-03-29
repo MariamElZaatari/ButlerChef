78c9c91320cbeff6782a51efc6e275299ba2b746
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-29 08:54:13 +0300

    SmallRecipe Widget Linked to Backend

--- butlerchef_frontend/lib/widgets/small_recipe.dart
+++ butlerchef_frontend/lib/widgets/small_recipe.dart
@@ -3,20 +3,40 @@ import 'package:butler_chef/widgets/top_recipe_bar.dart';
 import 'package:butler_chef/widgets/bottom_recipe_bar.dart';
 import 'package:butler_chef/screens/recipe_screen.dart';
 
-class SmallRecipe extends StatelessWidget {
+import '../models/recipe_model.dart';
+import '../services/recipe_service.dart';
+
+class SmallRecipe extends StatefulWidget {
   const SmallRecipe({
     Key? key,
   }) : super(key: key);
 
   @override
+  SmallRecipeState createState() => SmallRecipeState();
+}
+
+class SmallRecipeState extends State<SmallRecipe> {
+  late List<RecipeCardModel> recipes = <RecipeCardModel>[];
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, () async {
+      await RecipeService.getAllRecipes().then((data) {
+        setState(() {
+          recipes = data;
+        });
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final List<String> entries = <String>['A', 'B', 'C'];
-//    final List<int> colorCodes = <int>[600, 500, 100];
     return Center(
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.fromLTRB(15, 12, 0, 0),
-        itemCount: entries.length,
+        itemCount: recipes.length,
         itemBuilder: (BuildContext context, int index) {
           return Stack(
             children: [
@@ -27,6 +47,7 @@ class SmallRecipe extends StatelessWidget {
                 padding: const EdgeInsets.fromLTRB(18, 2, 18, 12),
                 decoration: const BoxDecoration(
                   image: DecorationImage(
+//                  TODO recipes[index].recipe.imageUrl
                     image: NetworkImage(
                         'https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZCUyMHBob3RvZ3JhcGh5fGVufDB8fDB8fA%3D%3D&w=1000&q=80'),
                     fit: BoxFit.cover,
@@ -55,44 +76,59 @@ class SmallRecipe extends StatelessWidget {
                       ], // red to yellow // repeats the gradient over the canvas
                     ),
                   )),
-          InkWell(
-          onTap: () => {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-          builder: (context) => const RecipeScreen()),
-          )
-          },
-          splashColor: Colors.grey,
-          child: Container(
-                height: 280,
-                width: 281,
-                margin: const EdgeInsets.fromLTRB(0, 0, 12, 0),
-                padding: const EdgeInsets.fromLTRB(18, 2, 12, 12),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const <Widget>[
-                    Expanded(
-                        child: Align(
-                          alignment: Alignment.topCenter,
-                          child: TopRecipeBar(starSize: 25.0,),
-                        ),
-                        flex: 1),
-                     Expanded(
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(''),
-                        ),
-                        flex: 1),
-                     Expanded(
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: BottomRecipeBar(titleSize: 29.0, infoSize: 18.0, titleWidth: 200.0,isSmall: true,),
-                        ),
-                        flex: 4),
-                  ],
-                ),
-              )),
+              InkWell(
+                  onTap: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RecipeScreen()),
+                        )
+                      },
+                  splashColor: Colors.grey,
+                  child: Container(
+                    height: 280,
+                    width: 281,
+                    margin: const EdgeInsets.fromLTRB(0, 0, 12, 0),
+                    padding: const EdgeInsets.fromLTRB(18, 2, 12, 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Expanded(
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: TopRecipeBar(
+                                starSize: 25.0,
+                                fridge: recipes[index].fridge,
+                                total: recipes[index].total,
+                                rate: recipes[index].recipe?.rate,
+                              ),
+                            ),
+                            flex: 1),
+                        const Expanded(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(''),
+                            ),
+                            flex: 1),
+                        Expanded(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: BottomRecipeBar(
+                                titleSize: 29.0,
+                                infoSize: 18.0,
+                                titleWidth: 200.0,
+                                isSmall: true,
+                                name: recipes[index].recipe?.name,
+                                level: recipes[index].recipe?.level,
+                                serving: recipes[index].recipe?.serving,
+                                time: recipes[index].recipe?.time,
+                                user: recipes[index].recipe?.user,
+                              ),
+                            ),
+                            flex: 4),
+                      ],
+                    ),
+                  )),
             ],
           );
         },
