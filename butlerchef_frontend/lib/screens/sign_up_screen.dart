1e60bc1dc39f4c685492c0f58ae6c842c50c6034
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-31 00:52:30 +0300

    Validation and Error Handling Added to SignUpScreen

--- butlerchef_frontend/lib/screens/sign_up_screen.dart
+++ butlerchef_frontend/lib/screens/sign_up_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:butler_chef/constants/app_colors.dart';
 import 'package:butler_chef/widgets/input.dart';
 import 'package:butler_chef/screens/login_screen.dart';
+import '../constants/styles.dart';
 import '../services/auth_service.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -9,11 +10,10 @@ class SignUpScreen extends StatefulWidget {
 
   @override
   _SignUpScreenState createState() => _SignUpScreenState();
-
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-
+  final _formKey = GlobalKey<FormState>();
   String email = "";
   String password = "";
   String repeatPassword = "";
@@ -50,86 +50,136 @@ class _SignUpScreenState extends State<SignUpScreen> {
     });
   }
 
+  void _showAlertDialog(String message) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.red,
+          title: Text(
+            message,
+            style: ThemeText.errorMessage,
+            textAlign: TextAlign.center,
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.logoBackground,
       body: Center(
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 18.0),
-            child: ListView(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(21,61,21,0),
-                    width: MediaQuery.of(context).size.width,
-                    height: 141,
-                    child: Image.asset(
-                      'assets/images/logo.png',
-                      fit: BoxFit.contain,
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 18.0),
+        child: ListView(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(21, 0, 21, 0),
+                width: MediaQuery.of(context).size.width,
+                height: 141,
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 1,
+                child: Column(children: const [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 41, 0, 0),
+                    child: Text(
+                      "Let's Get Started",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: AppColors.black,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 2),
                     ),
-                  ),),
-                Expanded(
-                    flex: 1,
-                    child: Column(children: const [
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0, 41, 0, 0),
-                        child: Text(
-                          "Let's Get Started",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: AppColors.black,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 2),
-                        ),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 41),
-                          child: Text(
-                            'You are one click away from joining our food lovers community.',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: AppColors.black,
-                                fontSize: 21,
-                                fontWeight: FontWeight.w300,
-                                letterSpacing: 2),
-                          )),
-                    ])),
-                Expanded(
-                    flex: 2,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 41),
+                      child: Text(
+                        'You are one click away from joining our food lovers community.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: AppColors.black,
+                            fontSize: 21,
+                            fontWeight: FontWeight.w300,
+                            letterSpacing: 2),
+                      )),
+                ])),
+            Expanded(
+                flex: 2,
+                child: Form(
+                    key: _formKey,
                     child: Column(children: [
-                      Input(value: "Email", callback: setEmail, isPassword: false,),
-                      Input(value: "Password", callback: setPassword, isPassword: false,),
-                      Input(value: "Repeat Password", callback: setRepeatPassword,isPassword: false,),
-                      Input(value: "First Name", callback: setFirstName, isPassword: false,),
-                      Input(value: "Last Name", callback: setLastName, isPassword: false,),
-                    ])),
-                Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        Container(
-                            width: 295,
-                            height: 89,
-                            margin: const EdgeInsets.fromLTRB(0, 26, 0, 0),
-                            child: TextButton(
-                                style: TextButton.styleFrom(
-                                  padding: const EdgeInsets.all(21),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(25)),
-                                  backgroundColor: AppColors.darkGreen,
-                                ),
-                                child: const Text(
-                                  "Sign Up",
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w900,
-                                      letterSpacing: 2,
-                                      color: AppColors.white),
-                                ),
-                                onPressed: () {
+                  Input(
+                    value: "Email",
+                    callback: setEmail,
+                    isPassword: false,
+                  ),
+                  Input(
+                    value: "Password",
+                    callback: setPassword,
+                    isPassword: true,
+                  ),
+                  Input(
+                    value: "Repeat Password",
+                    callback: setRepeatPassword,
+                    isPassword: true,
+                    password:password,
+                  ),
+                  Input(
+                    value: "First Name",
+                    callback: setFirstName,
+                    isPassword: false,
+                  ),
+                  Input(
+                    value: "Last Name",
+                    callback: setLastName,
+                    isPassword: false,
+                  ),
+                ]))),
+            Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Container(
+                        width: 295,
+                        height: 89,
+                        margin: const EdgeInsets.fromLTRB(0, 41, 0, 0),
+                        child: TextButton(
+                            style: TextButton.styleFrom(
+                              padding: const EdgeInsets.all(21),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25)),
+                              backgroundColor: AppColors.darkGreen,
+                            ),
+                            child: const Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w900,
+                                  letterSpacing: 2,
+                                  color: AppColors.white),
+                            ),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+//                                Navigator.push(
+//                                  context,
+//                                  MaterialPageRoute(
+//                                    builder: (context) {
+//                                      return const LoginScreen();
+//                                    },
+//                                  ),
+//                                );
+
 
                                   AuthService.register(email, password, repeatPassword, firstName, lastName).then((response) {
 
@@ -146,40 +196,38 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
                                     }
                                     else {
-                                      print("Failed to Create a User");
+                              _showAlertDialog("Failed to Create a User");
                                     }
 
-                                  });
-
-                                }
-                            )),
-                        SizedBox(
-                            width: 295,
-                            height: 41,
-                            child: TextButton(
-                              child: const Text(
-                                "Already have an account?",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w400,
-                                    color: AppColors.black),
+                                  });}
+                            })),
+                    SizedBox(
+                        width: 295,
+                        height: 41,
+                        child: TextButton(
+                          child: const Text(
+                            "Already have an account?",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.black),
+                          ),
+                          onPressed: () => {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const LoginScreen();
+                                },
                               ),
-                              onPressed: () => {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return const LoginScreen();
-                                  },
-                                ),
-                              )
-                              },
-                            )),
-                      ],
-                    ))
-              ],
-            ),
-          )),
+                            )
+                          },
+                        )),
+                  ],
+                ))
+          ],
+        ),
+      )),
     );
   }
 //
