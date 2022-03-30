6fa6a9a90528b209ad1824abe0bef5dfa4687fcd
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-30 04:26:44 +0300

    createFavoriteByRecipeId and deleteFavoriteById Methods Added to FavoriteRecipeService

--- butlerchef_frontend/lib/services/favorite_recipe_service.dart
+++ butlerchef_frontend/lib/services/favorite_recipe_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:butler_chef/models/recipe_model.dart';
 import 'package:butler_chef/utils/app_url.dart';
 import 'package:http/http.dart';
@@ -19,5 +21,34 @@ class FavoriteRecipeService {
       return recipesCards;
     }
     return [];
+  }
+
+  static Future<bool> createFavoriteByRecipeId(int recipeId) async {
+    int? id = (await UserPreferences().getUser()).id ?? 0;
+
+    final Map<String, int> data = {'userId': id, 'recipeId': recipeId};
+
+    Response response = await post(
+      Uri.parse(AppUrl.createFavorite),
+      body: json.encode(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
+  }
+
+  static Future<bool> deleteFavoriteById(int recipeId) async {
+    Response response =
+    await get(Uri.parse(AppUrl.deleteFavorite + recipeId.toString()));
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
   }
 }
