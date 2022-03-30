22e50dc9783dd9b8c840e2841f712158eeab7902
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-30 04:27:25 +0300

    RecipeProductService Added

---
+++ butlerchef_frontend/lib/services/recipe_product_service.dart
@@ -0,0 +1,17 @@
+import 'package:butler_chef/utils/app_url.dart';
+import 'package:http/http.dart';
+import '../models/recipe_product_model.dart';
+
+class RecipeProductService {
+  static Future<List<RecipeProduct>> getProductsByRecipeID(int id) async {
+    Response response = await get(Uri.parse(AppUrl.getProductsByRecipeId + id.toString()));
+
+    if (response.statusCode == 200) {
+      List<RecipeProduct> products =
+          RecipeProductResponse.welcomeFromJson(response.body).data;
+
+      return products;
+    }
+    return [];
+  }
+}
