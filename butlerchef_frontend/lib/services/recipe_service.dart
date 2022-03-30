5b9bb6e5bb464a713d09c8462074a335d2a19af6
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-31 01:21:45 +0300

    RecipeService CreateRecipe Modified to Dynamic

--- butlerchef_frontend/lib/services/recipe_service.dart
+++ butlerchef_frontend/lib/services/recipe_service.dart
@@ -3,25 +3,20 @@ import 'package:butler_chef/constants/app_url.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
-import '../models/measurement_model.dart';
-import '../models/quantity_model.dart';
 import '../models/recipe_direction_model.dart';
 import '../models/recipe_product_model.dart';
 import '../utils/shared_preference.dart';
 
 class RecipeService {
-
   static Future<List<RecipeCardModel>> getAllRecipes() async {
-
     int? loggedUserId = (await UserPreferences().getUser()).id;
 
-    Response response =
-    await get(Uri.parse(AppUrl.getAllRecipesByUserId + loggedUserId.toString()));
+    Response response = await get(
+        Uri.parse(AppUrl.getAllRecipesByUserId + loggedUserId.toString()));
 
     if (response.statusCode == 200) {
-
       List<RecipeCardModel> recipesCards =
-      RecipeCardModel.welcomeFromJson(response.body);
+          RecipeCardModel.welcomeFromJson(response.body);
 
       // print(recipesCards);
       return recipesCards;
@@ -29,51 +24,46 @@ class RecipeService {
     return [];
   }
 
+  static Future<List<RecipeCardModel>> getRecipesForLoggedUser() async {
+    int? loggedUserId = (await UserPreferences().getUser()).id;
+
+    Response response = await get(
+        Uri.parse(AppUrl.getAllRecipesByUserId + loggedUserId.toString()));
+
+    if (response.statusCode == 200) {
+      List<RecipeCardModel> tempRecipesCards =
+          RecipeCardModel.welcomeFromJson(response.body);
+
+      List<RecipeCardModel> recipesCards = <RecipeCardModel>[];
+
+      for (var card in tempRecipesCards) {
+        if (card.recipe?.user?.id == loggedUserId) {
+          recipesCards.add(card);
+        }
+      }
+
+      return recipesCards;
+    }
+
+    return [];
+  }
+
   static Future<bool> createRecipe(
-      // RecipeModel recipe,
-      // List<RecipeProduct> products,
-      // List<RecipeDirection> directions,
-      ) async {
+    RecipeModel? recipe,
+    List<RecipeProduct>? products,
+    List<RecipeDirection>? directions,
+  ) async {
+    recipe?.user = await UserPreferences().getUser();
 
-    int? userId = (await UserPreferences().getUser()).id;
-
-    RecipeModel recipe = RecipeModel(
-        name: "Eggs",
-        level: "hard",
-        time: "15 min",
-        serving: "5 srv",
-        visibility: 1);
-
-    List<RecipeProduct> products = <RecipeProduct>[
-      RecipeProduct(
-        quantity: Quantity(id: 1, value: "4"),
-        measurement: Measurement(id: 1, value: "4"),
-        name: "mele7",
-      ),
-      RecipeProduct(
-        quantity: Quantity(id: 1, value: "4"),
-        measurement: Measurement(id: 1, value: "4"),
-        name: "bhar",
-      ),
-    ];
-
-
-    List<RecipeDirection> directions = <RecipeDirection>[
-      RecipeDirection(title: "step 1", content: "prepare everything"),
-      RecipeDirection(title: "step 2", content: "cook cook"),
-    ];
-
-
-    final Map<String, dynamic> fridgeProduct = {
-      'userId': userId,
-      'recipe': json.encode(recipe),
-      'products': json.encode(products),
-      'directions': json.encode(directions),
+    final Map<String, dynamic> recipeCard = {
+      'recipe': recipe,
+      'products': products,
+      'directions': directions,
     };
 
     Response response = await post(
       Uri.parse(AppUrl.createRecipes),
-      body: json.encode(fridgeProduct),
+      body: json.encode(recipeCard),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -82,8 +72,6 @@ class RecipeService {
     if (response.statusCode == 200) {
       return true;
     }
-
     return false;
   }
-
 }
