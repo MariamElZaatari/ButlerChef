b9f7c7a5abc8371bdd245cedfb46f773513a8b86
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-30 04:27:09 +0300

    RecipeDirectionService Added

---
+++ butlerchef_frontend/lib/services/recipe_direction_service.dart
@@ -0,0 +1,17 @@
+import 'package:butler_chef/models/recipe_direction_model.dart';
+import 'package:butler_chef/utils/app_url.dart';
+import 'package:http/http.dart';
+
+class RecipeDirectionService {
+  static Future<List<RecipeDirection>> getDirectionsByRecipeID(int id) async {
+    Response response = await get(Uri.parse(AppUrl.getDirectionsByRecipeId + id.toString()));
+
+    if (response.statusCode == 200) {
+      List<RecipeDirection> directions =
+          RecipeDirectionResponse.welcomeFromJson(response.body).data;
+
+      return directions;
+    }
+    return [];
+  }
+}
