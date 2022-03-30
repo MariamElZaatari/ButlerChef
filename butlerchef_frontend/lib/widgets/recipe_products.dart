1b1fd2d4919e736f333fe6c8746517f6ab87a99f
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-30 10:30:49 +0300

    RecipeProducts Linked to Backend

--- butlerchef_frontend/lib/widgets/recipe_products.dart
+++ butlerchef_frontend/lib/widgets/recipe_products.dart
@@ -2,14 +2,31 @@ import 'package:butler_chef/constants/app_colors.dart';
 import 'package:butler_chef/widgets/product_item.dart';
 import 'package:flutter/material.dart';
 
+import '../models/recipe_product_model.dart';
+import '../services/recipe_product_service.dart';
+
 class RecipeProducts extends StatefulWidget {
-  const RecipeProducts({Key? key}) : super(key: key);
+  final int? recipeId;
+  const RecipeProducts({Key? key, required this.recipeId}) : super(key: key);
 
   @override
   RecipeProductsState createState() => RecipeProductsState();
 }
 
 class RecipeProductsState extends State<RecipeProducts> {
+  late List<RecipeProduct> products=[];
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, () async {
+      await RecipeProductService.getProductsByRecipeID(widget.recipeId).then((data) {
+        setState(() {
+          products = data;
+        });
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +37,10 @@ class RecipeProductsState extends State<RecipeProducts> {
             const Divider(
               color: AppColors.backgroundColor,
             ),
-            itemCount: 10,
+            itemCount: products.length,
             itemBuilder: (BuildContext context, int index) {
-              return const ProductItem(
-                  productName: "Name", quantity: "1", measurement: "Dz");
+              return ProductItem(
+                  productName: products[index].name, quantity: products[index].quantity.value, measurement: products[index].measurement.value);
             }));
   }
 }
