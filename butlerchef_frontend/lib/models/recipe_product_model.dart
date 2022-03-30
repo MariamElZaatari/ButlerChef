959eafa2e4cbfed147634ce14f2070a3ffaebb6f
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-30 04:23:38 +0300

    RecipeProductResponse Class Added to RecipeProductModel

--- butlerchef_frontend/lib/models/recipe_product_model.dart
+++ butlerchef_frontend/lib/models/recipe_product_model.dart
@@ -1,5 +1,33 @@
+import 'dart:convert';
+
 import 'package:butler_chef/models/quantity_model.dart';
 import 'package:butler_chef/models/measurement_model.dart';
+
+class RecipeProductResponse {
+  RecipeProductResponse({
+    required this.data,
+    required this.status,
+  });
+
+  List<RecipeProduct> data;
+  int status;
+
+  static RecipeProductResponse welcomeFromJson(String str) =>
+      RecipeProductResponse.fromJson(json.decode(str));
+
+  static String welcomeToJson(RecipeProductResponse data) =>
+      json.encode(data.toJson());
+
+  factory RecipeProductResponse.fromJson(Map<String, dynamic> json) => RecipeProductResponse(
+    data: List<RecipeProduct>.from(json["data"].map((x) => RecipeProduct.fromJson(x))),
+    status: json["status"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "data": List<dynamic>.from(data.map((x) => x.toJson())),
+    "status": status,
+  };
+}
 
 class RecipeProduct {
   RecipeProduct({
