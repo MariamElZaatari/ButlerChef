661fe516d888ddcbf8beb1a3c9c44c317131ecd3
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-30 21:11:12 +0300

    TextObscured if input is password and design of input modified

--- butlerchef_frontend/lib/widgets/input.dart
+++ butlerchef_frontend/lib/widgets/input.dart
@@ -4,8 +4,9 @@ import 'package:butler_chef/constants/app_colors.dart';
 class Input extends StatefulWidget {
   final String _hint;
   final Function(String) callback;
+  final bool isPassword;
 
-  const Input({Key? key, required value, required this.callback})
+  const Input({Key? key, required value, required this.callback, required this.isPassword})
       : _hint = value,
         super(key: key);
 
@@ -24,20 +25,29 @@ class _InputState extends State<Input> {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-          color: Colors.white,
           borderRadius: BorderRadius.all(Radius.circular(15))),
       width: 295,
       height: 55,
       margin: const EdgeInsets.fromLTRB(0, 0, 0, 12),
       child: Center(
         child: TextField(
+          obscureText: widget.isPassword? true:false,
           controller: inputValue,
           onChanged: (text) {
             widget.callback(text);
           },
           textAlign: TextAlign.center,
           decoration: InputDecoration(
-              border: InputBorder.none,
+              enabledBorder:
+              UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: AppColors.black),
+              ),
+              focusedBorder:
+              UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: AppColors.green),
+              ),
               hintText: widget._hint,
               hintStyle: const TextStyle(
                   color: AppColors.placeholder,
