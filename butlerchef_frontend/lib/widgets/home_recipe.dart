7c73dbbdde1197509abe27646c4203642c6c7f89
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-31 08:20:23 +0300

    HomeRecipe Widget added with searchBar and largeRecipe

---
+++ butlerchef_frontend/lib/widgets/home_recipe.dart
@@ -0,0 +1,58 @@
+import 'package:butler_chef/widgets/large_recipe.dart';
+import 'package:butler_chef/widgets/search_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:butler_chef/models/recipe_model.dart';
+import 'package:butler_chef/services/recipe_service.dart';
+
+class HomeRecipe extends StatefulWidget {
+  const HomeRecipe({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  HomeRecipeState createState() => HomeRecipeState();
+}
+
+class HomeRecipeState extends State<HomeRecipe> {
+  late List<RecipeCardModel> recipes = <RecipeCardModel>[];
+  String recipeName = "";
+
+  setRecipeName(newName) {
+    setState(() {
+      recipeName = newName;
+      initState();
+    });
+  }
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, () async {
+      if(recipeName.isNotEmpty){
+      await RecipeService.getRecipesByRecipeName(recipeName).then((data) {
+        setState(() {
+          recipes = data;
+        });
+      });
+      } else{
+        await RecipeService.getAllRecipes().then((data) {
+          setState(() {
+            recipes = data;
+          });
+        });
+
+      }
+      print(recipes.length);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SearchBar(callback: setRecipeName),
+        LargeRecipe(recipes: recipes)
+      ],
+    );
+  }
+}
