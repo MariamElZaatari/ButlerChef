345efc64851d42cd9c50daf1ef8b55e5e4fb6e89
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-31 09:05:39 +0300

    BodyWidget changed from stateless to stateful widget

--- butlerchef_frontend/lib/widgets/body_widget.dart
+++ butlerchef_frontend/lib/widgets/body_widget.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-import 'package:butler_chef/widgets/search_bar.dart';
 
-class BodyWidget extends StatelessWidget {
+class BodyWidget extends StatefulWidget {
   const BodyWidget({
     Key? key,
     required List<Widget> widgetOptions,
     required int selectedIndex,
-  })  : _widgetOptions = widgetOptions,
+  })
+      : _widgetOptions = widgetOptions,
         _selectedIndex = selectedIndex,
         super(key: key);
 
@@ -15,23 +15,20 @@ class BodyWidget extends StatelessWidget {
   final int _selectedIndex;
 
   @override
+  BodyWidgetState createState() => BodyWidgetState();
+}
+
+
+class BodyWidgetState extends State<BodyWidget> {
+  @override
   Widget build(BuildContext context) {
     return Container(
-      margin: _selectedIndex == 0? EdgeInsets.fromLTRB(0, 30, 0, 15): EdgeInsets.fromLTRB(0, 0, 0, 15),
-      child: Column(
-//        crossAxisAlignment: CrossAxisAlignment.start,
-//        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (_selectedIndex == 0)
-          SearchBar(),
-          Expanded(
-            flex: 9,
-            child: Center(
-              child: _widgetOptions.elementAt(_selectedIndex),
+      margin: widget._selectedIndex == 0? EdgeInsets.fromLTRB(0, 30, 0, 15): EdgeInsets.fromLTRB(0, 0, 0, 15),
+      child:
+            Center(
+              child: widget._widgetOptions.elementAt(widget._selectedIndex),
             ),
-          ),
-        ],
-      ),
+
     );
   }
 }
