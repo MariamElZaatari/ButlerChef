21d1a3c5b70ee3bbdbcbb3d8ec3ace647170570c
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-31 14:41:51 +0300

    AddFridgeProducts Fixed with Measurements and Quantities, and Linked to backend

--- butlerchef_frontend/lib/widgets/add_fridge_products.dart
+++ butlerchef_frontend/lib/widgets/add_fridge_products.dart
@@ -1,13 +1,15 @@
 import 'package:butler_chef/constants/app_colors.dart';
+import 'package:butler_chef/services/fridge_product_service.dart';
 import 'package:butler_chef/widgets/add_products_item.dart';
 import 'package:flutter/material.dart';
-import 'package:butler_chef/models/measurement_quantity_model.dart';
 import 'package:butler_chef/models/measurement_model.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../models/quantity_model.dart';
 import '../models/recipe_product_model.dart';
 import 'package:butler_chef/constants/styles.dart';
+
+import '../screens/home_screen.dart';
 
 class AddFridgeProducts extends StatefulWidget {
   const AddFridgeProducts({
@@ -25,6 +27,7 @@ class AddFridgeProducts extends StatefulWidget {
 class AddFridgeProductsState extends State<AddFridgeProducts>
     with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
   late final List<RecipeProduct> _products = [];
+  late final List<List<Quantity>> _listOfQuantities = [];
   late final Animation<double> _animation;
 
   @override
@@ -32,6 +35,22 @@ class AddFridgeProductsState extends State<AddFridgeProducts>
     super.initState();
     _products.addAll(widget.products);
     _animation = AnimationController(vsync: this, value: 1);
+  }
+
+  void _showAlertDialog(String message) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.red,
+          title: Text(
+            message,
+            style: ThemeText.errorMessage,
+            textAlign: TextAlign.center,
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -48,50 +67,106 @@ class AddFridgeProductsState extends State<AddFridgeProducts>
           if (index == _products.length) {
             return Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: ElevatedButton(
-                    onPressed: _addIngredient,
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50.0))),
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(AppColors.green),
-                      overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                        (Set<MaterialState> states) {
-                          if (states.contains(MaterialState.focused) ||
-                              states.contains(MaterialState.pressed)) {
-                            return AppColors.white.withOpacity(0.12);
-                          }
-                          return null;
-                        },
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                        onPressed: _addIngredient,
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(50.0))),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(AppColors.green),
+                          overlayColor:
+                              MaterialStateProperty.resolveWith<Color?>(
+                            (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.focused) ||
+                                  states.contains(MaterialState.pressed)) {
+                                return AppColors.white.withOpacity(0.12);
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        child: RichText(
+                            text: const TextSpan(children: [
+                          WidgetSpan(
+                            child: FaIcon(FontAwesomeIcons.plus,
+                                color: AppColors.white, size: 21),
+                          ),
+                          TextSpan(
+                              text: " Add Ingredient",
+                              style: ThemeText.buttonText)
+                        ]))),
+                    Container(
+                      height: 21.0,
+                      width: 321,
+                      margin: const EdgeInsets.only(bottom: 21, right: 30),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: FittedBox(
+                            child: IconButton(
+                          onPressed: () async {
+                            bool success = false;
+                            for (var fridgeProduct in _products) {
+                              success = (await FridgeProductService
+                                  .createFridgeProduct(
+                                      fridgeProduct.quantity.id,
+                                      fridgeProduct.quantity.value,
+                                      fridgeProduct.measurement.id ?? 0,
+                                      fridgeProduct.measurement.value ?? "",
+                                      fridgeProduct.name));
+                            }
+                            success
+                                ? Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) {
+                                        return const HomeScreen();
+                                      },
+                                    ),
+                                  )
+                                : _showAlertDialog("Failed to add Fridge Product.");
+                          },
+                          icon: const Icon(
+                            Icons.check_circle_rounded,
+                            color: AppColors.green,
+                            size: 121,
+                          ),
+                        )),
                       ),
                     ),
-                    child: RichText(
-                        text: const TextSpan(children: [
-                      WidgetSpan(
-                        child: FaIcon(FontAwesomeIcons.plus,
-                            color: AppColors.white, size: 21),
-                      ),
-                      TextSpan(
-                          text: " Add Ingredient", style: ThemeText.buttonText)
-                    ]))));
+                  ],
+                ));
           }
           final item = _products[index];
+          final quantityItem = _listOfQuantities[index];
           Widget child = AddProductsItem(
             key: UniqueKey(),
             name: item.name,
+            currentMeasurement: item.measurement,
+            currentQuantity: item.quantity,
+            currentQuantityList: quantityItem,
             animation: _animation,
             onClicked: () => _removeIngredient(index),
             onNameChange: (name) => _onItemNameChange(index, name),
-            onMeasurementChange: (measurement) =>
-                _onItemMeasurementChange(index, measurement),
+            onMeasurementChange: (Measurement m) =>
+                _onItemMeasurementChange(index, m),
             onQuantityChange: (quantity) =>
                 _onItemQuantityChange(index, quantity),
+            onQuantityListChange: (quantities) =>
+                _setQuantitiesList(index, quantities),
           );
           return child;
         },
       ),
     );
+  }
+
+  void _setQuantitiesList(int i, List<Quantity> q) {
+    _listOfQuantities[i] = q;
   }
 
   void _addIngredient() {
@@ -102,43 +177,42 @@ class AddFridgeProductsState extends State<AddFridgeProducts>
         quantity: Quantity(id: 1, value: "1"),
       ),
     );
+    _listOfQuantities.add(<Quantity>[Quantity(id: 1, value: "1")]);
     setState(() {});
   }
 
   void _removeIngredient(int index) {
     _products.removeAt(index);
+    _listOfQuantities.removeAt(index);
     setState(() {});
   }
 
   void _onItemNameChange(int index, String name) {
-    _onItemChange(index, name: name);
+    _onItemChange(index, tempName: name);
   }
 
-  void _onItemMeasurementChange(
-      int index, MeasurementWithQuantities measurement) {
-    _onItemChange(index, measurement: measurement);
+  void _onItemMeasurementChange(int index, Measurement m) {
+    _onItemChange(index, measurement: m);
   }
 
   void _onItemQuantityChange(int index, Quantity quantity) {
     _onItemChange(index, quantity: quantity);
   }
 
-  void _onItemChange(
-    int index, {
-    String? name,
-    MeasurementWithQuantities? measurement,
-    Quantity? quantity,
-  }) {
-    print('call');
+  void _onItemChange(int index,
+      {String? tempName, Measurement? measurement, Quantity? quantity}) {
     setState(() {
-      final item = _products[index];
-      _products[index] = RecipeProduct(
-          name: name ?? item.name,
-          quantity: quantity ?? item.quantity,
-          measurement: Measurement.fromMeasurementWithQuantity(measurement));
+      // RecipeProduct item = _ingredients[index];
+      if (tempName != null) {
+        _products[index].name = tempName;
+      }
+      if (measurement != null) {
+        _products[index].measurement = measurement;
+      }
+      if (quantity != null) {
+        _products[index].quantity = quantity;
+      }
     });
-    print(_products[index].name);
-    widget.onProductsChange?.call(_products);
   }
 
   @override
