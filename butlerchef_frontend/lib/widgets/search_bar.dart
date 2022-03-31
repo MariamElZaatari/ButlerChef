1fddb385a1c85c66436ecb5056ed7f641805f13a
MariamElZaatari <mariamalzaatari@gmail.com>
2022-03-31 08:01:57 +0300

    SearchBar changed to stateful widget with callback function

--- butlerchef_frontend/lib/widgets/search_bar.dart
+++ butlerchef_frontend/lib/widgets/search_bar.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:butler_chef/constants/app_colors.dart';
 
-class SearchBar extends StatelessWidget {
+class SearchBar extends StatefulWidget {
+  final Function(String) callback;
+
   const SearchBar({
-    Key? key,
+    Key? key, required this.callback,
   }) : super(key: key);
 
+  @override
+  SearchBarState createState() => SearchBarState();
+}
+
+class SearchBarState extends State<SearchBar> {
+late final TextEditingController _controller;
+
+void initState() {
+  super.initState();
+  _controller = TextEditingController(text: "");
+}
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,15 +28,17 @@ class SearchBar extends StatelessWidget {
           borderRadius: BorderRadius.all(Radius.circular(20))),
       width: double.infinity,
       height: 40,
-      child: const Center(
+      child: Center(
         child: TextField(
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             border: InputBorder.none,
             hintText: 'Search recipes...',
             prefixIcon: Icon(Icons.search_rounded,
                 color: AppColors.green,
           ),
         ),
+    controller: _controller,
+    onSubmitted: (text) => widget.callback(_controller.text),
       ),
     ),
     );
