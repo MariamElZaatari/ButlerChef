c6f5c3a14994255483d18a9d0bbfc45db8a3eb74
MariamElZaatari <mariamalzaatari@gmail.com>
2022-04-06 02:51:00 +0300

    MeasurementQuantity Model Cleaned

--- butlerchef_frontend/lib/models/measurement_quantity_model.dart
+++ butlerchef_frontend/lib/models/measurement_quantity_model.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:butler_chef/models/quantity_model.dart';
 
 class MeasurementResponse {
@@ -11,21 +9,17 @@ class MeasurementResponse {
   List<MeasurementWithQuantities> measurement;
   int? status;
 
-  // static MeasurementResponse welcomeFromJson(String str) =>
-  //     MeasurementResponse.fromJson(json.decode(str));
-  //
-  // static String welcomeToJson(MeasurementResponse data) =>
-  //     json.encode(data.toJson());
-
-  factory MeasurementResponse.fromJson(Map<String, dynamic> json) => MeasurementResponse(
-    measurement: List<MeasurementWithQuantities>.from(json["measurement"].map((x) => MeasurementWithQuantities.fromJson(x))),
-    status: json["status"],
-  );
+  factory MeasurementResponse.fromJson(Map<String, dynamic> json) =>
+      MeasurementResponse(
+        measurement: List<MeasurementWithQuantities>.from(json["measurement"]
+            .map((x) => MeasurementWithQuantities.fromJson(x))),
+        status: json["status"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "measurement": List<dynamic>.from(measurement.map((x) => x.toJson())),
-    "status": status,
-  };
+        "measurement": List<dynamic>.from(measurement.map((x) => x.toJson())),
+        "status": status,
+      };
 }
 
 class MeasurementWithQuantities {
@@ -39,15 +33,17 @@ class MeasurementWithQuantities {
   List<Quantity> quantities;
   String? value;
 
-  factory MeasurementWithQuantities.fromJson(Map<String, dynamic> json) => MeasurementWithQuantities(
-    id: json["id"],
-    value: json["value"],
-    quantities: List<Quantity>.from(json["quantities"].map((x) => Quantity.fromJson(x))),
-  );
+  factory MeasurementWithQuantities.fromJson(Map<String, dynamic> json) =>
+      MeasurementWithQuantities(
+        id: json["id"],
+        value: json["value"],
+        quantities: List<Quantity>.from(
+            json["quantities"].map((x) => Quantity.fromJson(x))),
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "value": value,
-    "quantities": List<dynamic>.from(quantities.map((x) => x.toJson())),
-  };
+        "id": id,
+        "value": value,
+        "quantities": List<dynamic>.from(quantities.map((x) => x.toJson())),
+      };
 }
